83c40df54c3d6099b35dca05b0c83bcc2f215606
glimatecnologia <glimatecnologia@gmail.com>
2016-01-25 12:08:34 -0300

    construção do menu do nosso sistema
    podem comentar e dar sugestão

--- menu.php
+++ menu.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Menu do Sistema: Glima Tecnologia</title>
+<link href="css/estilo.css" rel="stylesheet" type="text/css" /> <!--chamando o CSS-->
+</head> <!--fim do cabeçalho-->
 
+<body>
+<div id="menu"> <!--Início da div do menu-->
+    	<h1>Gerenciador</h1><!--Gerenciador no menu-->
+        <ul>
+        	<li><a href="index.php">Inicio</a></li>
+            <li><a href="sair.php">Sair</a></li>
+            <li><a href="meus_dados.php">Editar Dados</a></li>
+        </ul>
+        <h1>Listagens</h1> <!--Listagem do menu-->
+        <ul>
+        	<li><a href="produtos.php">Produtos</a></li>
+            <li><a href="clientes.php">Clientes</a></li>
+            <li><a href="vendas.php">Vendas</a></li>
+            <li><a href="entradas.php">Estoque</a></li>
+        </ul>
+        <h1>Cadastros</h1><!--Opção cadastros no menu-->
+        <ul>
+        	<li><a href="cad_produtos.php">Produtos</a></li>
+            <li><a href="cad_clientes.php">Clientes</a></li>
+            <li><a href="venda_iniciada.php">Iniciar uma venda</a></li>
+        </ul>
+        <h1>Relátorios</h1><!--Opção Relatórios no menu-->
+        <ul>
+        	<li><a href="rela_produtos.php" target="_blank">Produtos</a></li>
+            <li><a href="rela_clientes.php" target="_blank">Clientes</a></li>
+            <li><a href="rela_vendas.php" target="_blank">Vendas</a></li>
+            <li><a href="emite_recibos.php">Emissão de recibos</a></li>
+        </ul>
+</div><!--fim da construção do menu-->
+ 	
+</body> <!--fim do body-->
+</html><!--fim do html-->
